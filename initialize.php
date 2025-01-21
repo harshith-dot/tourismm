451b103c3cd62a65df409b1086d07d42a55ca518
harshithvadlamudi <harshith_vadlamudi@srmap.edu.in>
2025-01-22 02:06:17 +0530

    2nd

--- initialize.php
+++ initialize.php
@@ -3,8 +3,8 @@ $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=
 if(!defined('base_url')) define('base_url','http://localhost/tourism/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"tourism_db");
+if(!defined('DB_SERVER')) define('DB_SERVER',"db4free.net");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"tourism1");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Harshith");
+if(!defined('DB_NAME')) define('DB_NAME',"tourism1");
 ?>
